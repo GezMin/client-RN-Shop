c67c3f914ef95ab1bc21005d58d3c7a96a38b43e
GezMin <s.shvetsov.v@gmail.com>
2024-06-03 23:44:15 +0500

    import dependencies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 
 export default function App() {
     return (
-        <View style={styles.container}>
-            <Text>Start!!!</Text>
-            <StatusBar style='auto' />
-        </View>
+        <>
+            <SafeAreaProvider>
+                <Text>App</Text>
+            </SafeAreaProvider>
+        </>
     )
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-})
