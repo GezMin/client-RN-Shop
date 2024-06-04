693cc27c3a5b2aa963a8106275afbcc864d2d08d
GezMin <s.shvetsov.v@gmail.com>
2024-06-04 21:53:07 +0500

    added login fields

--- app/components/screens/auth/Auth.tsx
+++ app/components/screens/auth/Auth.tsx
@@ -1,11 +1,52 @@
+import AuthFields from './AuthFields'
+import Loader from '@/components/ui/Loader'
+import Button from '@/components/ui/button/Button'
+import { IAuthFormData } from '@/types/auth.interface'
 import { FC, useState } from 'react'
-import { Text, View } from 'react-native'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { Pressable, Text, View } from 'react-native'
 
 const Auth: FC = () => {
 	const [isReg, setIsReg] = useState(false)
+
+	const { handleSubmit, reset, control } = useForm<IAuthFormData>({
+		mode: 'onChange'
+	})
+
+	const onSubmit: SubmitHandler<IAuthFormData> = data => {
+		console.log(data)
+	}
+
+	const isLoading = false
+
 	return (
-		<View>
-			<Text>Auth</Text>
+		<View className='mx-2 items-center justify-center h-full'>
+			<View className='w-9/12'>
+				<Text className='text-center text-black text-3xl font-medium mb-8'>
+					{isReg ? 'Sing Up' : 'Login'}
+				</Text>
+				{isLoading ? (
+					<Loader />
+				) : (
+					<>
+						<AuthFields control={control} isPassRequired={isReg} />
+						<Button onPress={handleSubmit(onSubmit)}>
+							<Text>{isReg ? 'Sing Up' : 'Login'}</Text>
+						</Button>
+
+						<Pressable onPress={() => setIsReg(!isReg)}>
+							<Text className='text-black text-center text-base mt-6'>
+								{isReg
+									? 'Already have an account? '
+									: "Don't have an account? "}
+								<Text className='text-[#47AA52]'>
+									{isReg ? 'Login' : 'Sign up'}
+								</Text>
+							</Text>
+						</Pressable>
+					</>
+				)}
+			</View>
 		</View>
 	)
 }
---
+++ app/components/ui/button/Button.tsx
@@ -0,0 +1,26 @@
+import { IButton } from './button.interface'
+import cn from 'clsx'
+import { FC, PropsWithChildren } from 'react'
+import { Pressable, Text } from 'react-native'
+
+const Button: FC<PropsWithChildren<IButton>> = ({
+	children,
+	className,
+	...rest
+}) => {
+	return (
+		<Pressable
+			className={cn(
+				'self-center mt-3.5 bg-[#47AA52] w-full py-3 font-light rounded-lg',
+				className
+			)}
+			{...rest}
+		>
+			<Text className=' text-white text-center font-medium text-lg'>
+				{children}
+			</Text>
+		</Pressable>
+	)
+}
+
+export default Button
